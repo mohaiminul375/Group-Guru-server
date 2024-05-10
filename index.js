15ab54f24f2529f3700787035401794072f7a176
Mohaiminul375 <mohaiminul375@gmail.com>
2024-05-10 17:37:19 +0600

    create server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/',(req,res)=>{
+    res.send('server in working')
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
