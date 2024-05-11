ea66dc0bf45a41cef59e8b1a6825c09cee1d2f2a
Mohaiminul375 <mohaiminul375@gmail.com>
2024-05-11 14:43:06 +0600

    createsome get method

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
 console.log(process.env.DB_USER);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://group_gure:7To2b6eDLFiBr4Pe@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,24 +24,55 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-   const assignmentCollection=client.db('group-guru').collection('all-assignment')
+    const assignmentCollection = client
+      .db("group-guru")
+      .collection("all-assignment");
+    const submissionCollection = client
+      .db("group-guru")
+      .collection("submitted-assignment");
     // get
-   app.get('/all-assignment',async(req,res)=>{
-    const result= await assignmentCollection.find().toArray()
-    res.send(result)
-   })
-   app.get('/all-assignment/:id',async(req,res)=>{
-    const id=req.params.id;
-    const query={_id:new ObjectId(id)}
-    const result= await assignmentCollection.findOne(query)
-    res.send(result)
-   })
-    // post
-    app.post('/all-assignment',async(req,res)=>{
-        const assignment=req.body;
-        const result = await assignmentCollection.insertOne(assignment);
-        res.send(result)
+    app.get("/all-assignment", async (req, res) => {
+      const result = await assignmentCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/all-assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.findOne(query);
+      res.send(result);
+    });
+    // get submitted assignment by user (must use jwt)
+    app.get('/submitted-assignment',async(req,res)=>{
+      let query={};
+      if(req.query?.email){
+        query={examinee_email:req.query.email}
+      }
+      const result= await submissionCollection.find(query).toArray()
+      res.send(result)
+    })
+    // get data by pending
+    //  app.get('/submitted-assignment',async(req,res)=>{
+    //   let query={};
+    //  })
 
+    // post
+    app.post("/all-assignment", async (req, res) => {
+      const assignment = req.body;
+      const result = await assignmentCollection.insertOne(assignment);
+      res.send(result);
+    });
+    // post all pending assignment
+    app.post("/submitted-assignment", async (req, res) => {
+      const submission = req.body;
+      const result = await submissionCollection.insertOne(submission);
+      res.send(result);
+    });
+    // delete
+    app.delete("/all-assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
